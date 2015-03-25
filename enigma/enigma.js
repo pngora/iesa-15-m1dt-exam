779013ce1e8b4ddeea60d6e4613d9f22b024c9a5
pngora <p.ngora@gmail.com>
2015-03-25 11:00:10 +0100

    J'ai rien compris

---
+++ enigma/enigma.js
@@ -0,0 +1,34 @@
+var enigma [
+        j :"a",
+        d :"b",
+        w :"c",
+        v :"d",
+        a :"d",
+        r :"e",
+        i :"f",
+        g :"g",
+        x :"h",
+        u :"i",
+        n :"j",
+        r :"k",
+        s :"l",
+        y :"m",
+        q :"n",
+        c :"o",
+        p :"p",
+     
+        f :"r",
+        m :"s",
+        z :"t",
+        t :"u",
+        k :"v",
+        h :"w",
+        b :"x",
+        e :"y",
+        l :"z"];
+
+/*var x;
+while (i < egnima.length) {
+    text += "Tle code est " + i;
+    i++;*/
+}
