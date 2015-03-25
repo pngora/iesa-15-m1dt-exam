1e93944c1423152315b5466e49c65bd04f329939
pngora <p.ngora@gmail.com>
2015-03-25 16:02:21 +0100

    reponse à la question

---
+++ oop/calculator.js
@@ -0,0 +1,9 @@
+
+var Calculator = {result : 0};
+
+add : function(parametre) {
+
+return 
+}
+
+document.getElementById("calculator").innerHTML = Calculator.add(0 + add);
---
+++ oop/oop.md
@@ -0,0 +1,7 @@
+Un Objet c'est :
+Des chaînes de caractères
+Des nombres
+Des tableaux
+Des fonctions
+Des instances de classe
+Une classe
