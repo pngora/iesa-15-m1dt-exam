e40a123c7317fbd11978ccd1adea79954311c173
pngora <p.ngora@gmail.com>
2015-03-25 12:47:50 +0100

    exam siteweb

---
+++ siteweb/detail.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+ <html lang ="fr">
+ 
+ <head>
+      <title>Le système solaire</title>
+      <meta charset ="UTF-8">
+      <link rel="stylesheet" type="text/css" href="mobile.css">
+      <link rel="stylesheet" type="text/css" href="desktop.css">
+ </head>
+
+<body>
+<body>
+<html>
---
+++ siteweb/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+ <html lang ="fr">
+ 
+ <head>
+      <title>Le système solaire</title>
+      <meta charset ="UTF-8">
+      <link rel="stylesheet" type="text/css" href="mobile.css">
+      <link rel="stylesheet" type="text/css" href="desktop.css">
+ </head>
+<body>
+	<div id="content">
+		<header>
+			<div id="title"> Le système solaire</div>
+			<div id="logo"><img src="logo.jpg" alt="systheme"  style="width:90px;"/></div>
+			
+		</header>
+
+		<nav>
+			<a href="#">Mercure</a>
+			<a href="#">Vénus</a>
+			<a href="#">Terre</a>
+			<a href="#">Mars</a>
+			<a href="#">Terre</a>
+			<a href="#">Jupiter</a>
+			<a href="#">Saturne</a>
+			<a href="#">Uranus</a>
+			<a href="#">Neptune</a>
+			<a href="#">Pluton</a>
+
+		</nav>
+
+		<section>
+			<div class="planete">
+				<img src="" alt="vénus">
+				<a href="#">
+
+			</div>
+
+		</section>
+		
+		<footer>
+		
+		</footer>
+	
+	</div>
+<body>
+<html>
